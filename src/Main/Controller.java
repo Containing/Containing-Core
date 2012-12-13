af724e04a3d427fd181ff65fe376b6e481d0724b
Martin <djmardijk@gmail.com>
2012-12-13 19:02:41 +0100

    Added some commets to the controller and TODO actions

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -71,15 +71,23 @@ public class Controller {
         //Er zijn in totaal  10 zeeschipkranen, 8 binnenvaartkranen, 4 treinkranen en 20 truckkranen 
         for(int i = 0; i < 10; i++)
         {    
+            // Add's 10 seaShipCranes
+            //craneList.add(Crane);
         }
         for(int i = 0 ; i < 8; i++)
-        {            
+        {     
+            // Add's 8 BargeCranes
+            //craneList.add(Crane);
         }        
         for(int i  =0 ; i < 4; i++)
-        {            
+        {         
+            // Add's 10 trianCranes
+            //craneList.add(Crane);
         }        
         for (int i = 0; i < 20; i++)
-        {            
+        {          
+            // Add's 20 truckCranes
+            //craneList.add(Crane);
         }
         
         // To do Generate all vehicles with their containers
@@ -96,11 +104,11 @@ public class Controller {
         for(Vehicle agv : agvList)
         {    
             // Index for the message list 
-            // so it don't start's the whole time at the beginning of the list
+            // so it doesn't start walking through each message
             int index = -1;
             
             // TEMP        
-            if( true)
+            if( index == -1)
             // When the current agv is active
             //if(agv.jobType != Vehicle.TypeJob.None)
             {
@@ -109,16 +117,16 @@ public class Controller {
             }
             else
             {
-                // When the agv has no task's walk through all the request
-                // To see if there is an assingment for an agv
+                // When this agv has no task's.
+                // Walk through all the messages for a job
                 while(++index < messages.size())
                 {
-                    // When the message requested vehicle equals the agv 
+                    // When the message requested an AGV 
                     if(messages.get(index).RequestedType() == agv.getClass())
                     {  
                         // Send's the agv to the node of the quest giver
                         agv.setDestination(messages.get(index).DestinationNode());
-                        // Removes the request
+                        // Removes the message
                         messages.remove(index--); 
                         // Stop looping because this agv now has an assingment
                         break;
@@ -130,12 +138,13 @@ public class Controller {
         // Updates the logic of each crane
         for(Vehicle crane : craneList)
         {
-            // Index for the message list so it doen
+            // Index for the message list 
+            // so it doesn't start walking through each message
             int index =-1;
-            // When the current crane is active
-            //TEMP
             
-            if(true)
+            //TEMP
+            if(index == -1)
+            // When the current crane is active
             //if(crane.jobType != Vehicle.TypeJob.None)
             {
                 // Updates Crane's logic
@@ -143,30 +152,29 @@ public class Controller {
             }
             else
             {
+                // When this crane has no task's.
+                // Walk through all the messages for a job
                 while(++index < messages.size())
                 {
-                    // When the message request equals the crane vehicletype
+                    // When the message request a crane of the same type of this crane
                     if(messages.get(index).RequestedType() == crane.getClass())
                     {
                         // Send's the crane to the node of the quest giver
                         crane.setDestination(messages.get(index).DestinationNode());
-                        
+                        // Request's an AGV for contianer pickup
                         messages.add(new Message(
                                 crane,
                                 AGV.class));
-                        // Removes the request
+                        // Removes this message
                         messages.remove(index--);
-                        // Stop looping because this agv now has an assingment
+                        // Stop looping because this crane now has an assingment
                         break;
                     }
                 }
-            }
-            
-            // When a crane need an AGV
-                //requestAGV.add(crane.node);
+            }            
         }
         
-        // Updates the logic of each vehicle
+        // Updates the logic of each docked vehicle
         for(Vehicle vehicle : dockedVehicles)
         {
             // Updates Vehicle logic
@@ -180,30 +188,35 @@ public class Controller {
      */
     public void VehicleDocking(Vehicle vehicle)
     {      
+        // Add the docked Vehicle
         dockedVehicles.add(vehicle);
-        
+        // Interger that holds the amount of cranes this vehicle can hold for loading and unloading
         int craneRequests = 0;
         
         //When this vehicle is a Train
         if(vehicle.getClass() == Train.class)
         {
+            // Train's can hold 2 cranes
             craneRequests = 2;
         }
         // When this vehicle is a Boat
         else if (vehicle.getClass() == Boat.class)
         {            
+            //TODO Boat crane request
+            //TODO 2 different types of boat's a SeaShip, Barge
         }
         // When this vehicle is a Truck
         else if (vehicle.getClass() == Truck.class)
         {
+            // Truck's can hold 1 crane
             craneRequests = 1;
         }
         
         // While the vehicle can hold more cranes 
         while(craneRequests-- != 0)
         { 
-            //messages.add(new Message(vehicle,Cranes.class)); 
-        
+            //Request's a crane for this vehicle to load or unload
+            //messages.add(new Message(vehicle,Cranes.class));         
         }
     }
 }
