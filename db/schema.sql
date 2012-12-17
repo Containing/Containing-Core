d21a8325ac182467f79c2819f42472bdb303436a
Christiaan Kras <c.kras@pcc-online.net>
2012-12-17 12:31:58 +0100

    Database schema (SQLite)

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+BEGIN TRANSACTION;
+CREATE TABLE `container` (
+    `id` TEXT PRIMARY KEY,
+    `arrivalDateStart` DATETIME,
+    `arrivalDateEnd` DATETIME,
+    `arrivalTransportType` INTEGER,
+    `arrivalCompany` TEXT,
+    `arrivalPosition` INTEGER,
+    `owner` TEXT,
+    `containerNr` INTEGER,
+    `departureDateStart` DATETIME,
+    `departureDateEnd` DATETIME,
+    `departureTransportType` INTEGER,
+    `departureCompany` TEXT,
+    `empty` INTEGER,
+    `weight` INTEGER,
+    `name` TEXT,
+    `kind` TEXT,
+    `danger` TEXT,
+    `locationId` INTEGER,
+    `storageLocation` INTEGER
+);
+CREATE UNIQUE INDEX idx_containerNr ON container(containerNr ASC);
+CREATE INDEX idx_dates ON container(arrivalDateStart ASC, arrivalDateEnd ASC, arrivalTransportType ASC);
+CREATE INDEX idx_departureDate ON container(departureDateStart ASC);
+CREATE INDEX idx_loc_stor ON container(locationId ASC, storageLocation DESC);
+COMMIT;
