abe1121589f4e8ebd03c78591a5d48975ace57f5
Martin <djmardijk@gmail.com>
2013-01-08 11:16:44 +0100

    Updated Controller and Message
    Added JavaDoc and made the controller more efficient

--- src/Helpers/Message.java
+++ src/Helpers/Message.java
@@ -13,32 +13,69 @@ import Vehicles.Vehicle;
  * @author Martin
  */
 public class Message {    
-    // Actiontypes for what the requestedObject need's to do   
+    // The actiontypes for what the requestedObject needs to do   
     public enum ACTION { Load, Unload, Fetch, Deliver }        
-    // Action for the requested object
+    // The action for the requested object
     ACTION action;
     
-    // Object that requested
+    // The object that's requested
     Object destinationObject;
-    // Object that is requested
+    // The object that's requested
     Object requestedObject;
-    // Container the fetch or deliver
-    Container contianer;
+    // The container that needs to be fetched or deliverd
+    Container container;
+    
+    /**
+     * Constructs a new message
+     * @param sourceSender The object that requests an object
+     * @param requestType The object that's requested
+     * @param action The action for the requested object
+     */
+    public Message(Object sourceSender, Object requestType,ACTION action)
+    {
+        this.destinationObject = sourceSender;
+        this.requestedObject = requestType;
+        this.action = action;
+    }
+    
+    /**
+     * Constructs a new message
+     * @param sourceSender The object that requests an object
+     * @param requestType The object that's requested
+     * @param action The action for the requested object
+     * @param container A container
+     */
+    public Message(Object sourceSender, Object requestType,ACTION action, Container container)
+    {
+        this.destinationObject = sourceSender;
+        this.requestedObject = requestType;
+        this.action = action;
+        this.container = container;
+    }
+    
+    /**
+     * The current action of the message
+     * @return The current action
+     */
+    public ACTION GetAction(){
+        return action;
+    }
     
     /**
      * When the current action is to deliver a container
-     * @return 
+     * @return True if Action is deliver, False otherwise
      */
     public boolean Deliver() throws Exception
     {
-        if(action != null)
-            throw new Exception("Action isn't initialized");            
+        if(action != null){
+            throw new Exception("Action isn't initialized");      
+        }
         return action.equals(ACTION.Deliver);
     }
     
     /**
      * When the current action is to fetch a container
-     * @return 
+     * @return True if Action is fetch, False otherwise
      */
     public boolean Fetch() throws Exception
     {
@@ -49,92 +86,76 @@ public class Message {
     
     /**
      * When the current action is to load a vehicle
-     * @return 
+     * @return True if Action is load, False otherwise
      */
     public boolean Load() throws Exception
     {
-        if(action != null)
+        if(action != null){
             throw new Exception("Action isn't initialized");  
+        }
         return action.equals(ACTION.Load);
     }
     
     /**
      * When the current action is to unload a vehicle
-     * @return 
+     * @return True if Action is unload, False otherwise
      */
     public boolean UnLoad() throws Exception
     {
-        if(action != null)
+        if(action != null){
             throw new Exception("Action isn't initialized");  
+        }
         return action.equals(ACTION.Unload);
     }
     
     /**
-     * Get's the object reference of the object that send the request
-     * @return 
+     * The destination node where the sender is stationd
+     * @return The destination node
      */
-    public Object DestinationObject() throws Exception
+    public Node DestinationNode() throws Exception
     {
-        if(destinationObject == null)            
-            throw new Exception("DestinationObject isn't initialized");        
-        return destinationObject;
-    }
-    
-    /**
-     * The destination where the requested object need's to go
-     * @return 
-     */
-    public Node destinationNode() throws Exception
-    {
-        if(destinationObject == null)
+        if(destinationObject == null){
              throw new Exception("No destinationNode initialized");
-        
-        //if(destinationObject.getClass() == (Crane))
-        //    return ((Crane)destination).position;
-        if(destinationObject.getClass() == Vehicle.class)
+        }
+        if(destinationObject.getClass() == Vehicle.class){
             return ((Vehicle)destinationObject).getDestination();    
-        
+        }        
         throw new Exception("Message send from unknown source");
     }
     
     /**
-     * The current action of this message
-     * @return the action
+     * Gets the container that needs to be fetched or deliverd
+     * @return The container
      */
-    public ACTION Action()
+    public Container GetContainer() throws Exception
     {
-        return action;
+        if(container == null){
+            throw new Exception("Container isn't initialized");
+        }
+        return container;
     }
     
     /**
-     * Object that is requested
-     * @return 
+     * Gets the object that send the request
+     * @return The destination object
+     */
+    public Object DestinationObject() throws Exception
+    {
+        if(destinationObject == null)      {     
+            throw new Exception("DestinationObject isn't initialized");
+        }
+        return destinationObject;
+    }
+    
+    /**
+     * Gets the object that's requested
+     * @return The requested object
      */
     public Object RequestedObject() throws Exception
     {
-        if(requestedObject == null)
+        if(requestedObject == null){
             throw new Exception("RequestedObject isn't initialized");
+        }
         return requestedObject.getClass();
-    }
-    
-    public Message(Object destination, ACTION action)
-    {
-        this.destinationObject = destination;
-        this.action = action;
-    }
-    
-    public Message(Object sourceSender, Object requestType,ACTION action)
-    {
-        this.destinationObject = sourceSender;
-        this.requestedObject = requestType;
-        this.action = action;
-    }
-    
-    public Message(Object sourceSender, Object requestType,ACTION action, Container container)
-    {
-        this.destinationObject = sourceSender;
-        this.requestedObject = requestType;
-        this.action = action;
-        this.contianer = container;
     }
 }
--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -21,47 +21,47 @@ import Crane.Crane;
  * @author Martin_Notebook
  */
 public class Controller {
-    // Updates the update method
+    // The updateTimer class, Updates the update method
     updateTimer timer;
     
-    List<Vehicle> dockedVehicles;
-    // List with all AGV's
+    // List with all the present transport vehicles
+    List<Vehicle> presentVehicles;
+    // List with all AGVs
     List<Vehicle> agvList;
     // List with all Cranes
     List<Crane> craneList;
-    // List with all current messages
-    List<Message> messages;
+    // List with all the messages for the controller
+    List<Message> messageQueue;
     
     // The storage area where all the containers are stored
     Storage_Area storageArea;
     
-    // Simulation Time
+    // The simulation time of this application
     Date simulationTime;
-    // Date when the controller needs to send containers
+    // The date when the next container needs to be send
     Date deliveryTime;
-    // Date when the next shipment comes in
+    // The date when the next shipment arrives
     Date nextShipment;
     
-    // List with all the Vehicles that can arrive
+    // List with all the Vehicles that will arrive
     List<Boat> allSeaShips;
     List<Boat> allBarges;
     List<Train> allTrains;
     List<Truck> allTrucks;
     
     /**
-     * The amount of seconds the simulation time increments after each update
+     * The amount of seconds the simulation time will increment after each update
      */
     public int SecondsIncrement;
     
     /**
-     * Class Constructor,
-     * Call's the initialize method
+     * Constructs a new controller
+     * Then initializes the controller
      */
     public Controller() throws Exception
     {
         // Initializes the class variables
-        Initialize();
-        
+        Initialize();        
         // Walk's through all the method's in this class
         for(Method method : this.getClass().getMethods()){
             // When the method equals the update method
@@ -70,8 +70,7 @@ public class Controller {
                 timer = new updateTimer(method, this);
                 break;
             }
-        }
-        
+        }        
         // Start's and run's the timer for updating the application
         timer.start();
         timer.run();
@@ -85,35 +84,36 @@ public class Controller {
         // Default increment value
         SecondsIncrement = 1;
         
-        // Initiallizes the List's variables
-        messages = new ArrayList();
-        dockedVehicles = new ArrayList();        
+        // Initializes new ArrayLists
+        messageQueue = new ArrayList();
+        presentVehicles = new ArrayList();        
         agvList = new ArrayList();
         craneList = new ArrayList();      
         
+        // Loads or creates a new database dump
         try{
             // Loads the database dump
             Database.restoreDump();
         }
-        catch(Exception ex)
-        {
+        catch(Exception ex){
             //XML.XMLBinder.GenerateContainerDatabase();
             Database.dumpDatabase();         
         }
+        
         // Loads all the vehicles that come to the harbor
         allSeaShips = GenerateVehicles.GetSeaBoats();
         allBarges = GenerateVehicles.GetInlandBoats();
         allTrains = GenerateVehicles.GetTrains();
         allTrucks = GenerateVehicles.GetTrucks();
         
-        // Sets the first shipment time
+        // Initializes the first shipment time
         GetNextShipmentTime();
             
-        // Sets the simulationTime 1 hour before the first cargo
+        // Sets the simulationTime 1 hour before the first shipment
         simulationTime = nextShipment;
         simulationTime.setHours(simulationTime.getHours() -1);
         
-        // Add's 100 AGV's to the class all waiting
+        // Adds 100 AGVs
         for(int i = 0; i < 100; i++){
             agvList.add(new AGV(new Node(1,0)));
         }
@@ -125,28 +125,25 @@ public class Controller {
         
         //Er zijn in totaal  10 zeeschipkranen, 8 binnenvaartkranen, 4 treinkranen en 20 truckkranen 
         for(int i = 0; i < 10; i++){    
-            // Add's 10 seaShipCranes
+            // Add 10 seaShipCranes
             //craneList.add(Crane);
         }
         for(int i = 0 ; i < 8; i++){     
-            // Add's 8 BargeCranes
+            // Add 8 BargeCranes
             //craneList.add(Crane);
         }        
         for(int i  =0 ; i < 4; i++){         
-            // Add's 10 trianCranes
+            // Add 4 trainCranes
             //craneList.add(Crane);
         }        
         for (int i = 0; i < 20; i++){          
-            // Add's 20 truckCranes
+            // Add 20 truckCranes
             //craneList.add(Crane);
         }
-        
-        // To do Generate all vehicles with their containers
-        
     }
     
     /**
-     * Updates class logic
+     * Updates simulation logic
      * @param gameTime 
      */
     public void Update(float gameTime ) throws Exception
@@ -164,7 +161,7 @@ public class Controller {
             crane.update(gameTime);
         }
         // Updates the logic of each docked vehicle
-        for(Vehicle vehicle : dockedVehicles){
+        for(Vehicle vehicle : presentVehicles){
             vehicle.update(gameTime);
         }        
         
@@ -172,11 +169,9 @@ public class Controller {
         if(simulationTime.getTime() >= nextShipment.getTime()){
             UpdateShipment();
             GetNextShipmentTime();
-        }
-        
+        }        
         // When the simulation time is equal or greater than the deliveryTime
-        if(simulationTime.getTime() >= deliveryTime.getTime())
-        {
+        if(simulationTime.getTime() >= deliveryTime.getTime()){
             // Gets all the top containers that need to be transported
             FetchContainers();
             // Gets the next date when the next shipment needs to be transported
@@ -187,24 +182,30 @@ public class Controller {
              * When a container is fetch check if the container below if it needs to be fetched
              */
         }
-        
-        // Check's every message
-        for(Message mess : messages)
-        {
-            // When this message is an agv request 
-            if(mess.RequestedObject() == AGV.class)
-            {  
-                for(Vehicle agv : agvList)
-                {
-                    // When the agv has nothing to do
-                    if(((AGV)agv).Available())
-                    {
-                        //Set's the destination of the AGV
-                        agv.setDestination(mess.destinationNode());
-                        //Copy's the message to the message que
-                        ((AGV)agv).SendMessage(mess);
+        // Updates all the messageQueue
+        UpdateMessages();
+    }
+    
+    /**
+     * Updates all the messageQueue 
+     * @throws Exception 
+     */
+    private void UpdateMessages() throws Exception
+    {
+        // Checks every message
+        for(Message message : messageQueue){
+            // When the message requests an AGV 
+            if(message.RequestedObject() == AGV.class){  
+                // Checks every agv if it's available
+                for(Vehicle agv : agvList){
+                    // When the agv is available
+                    if(((AGV)agv).Available()){
+                        //Sets the destination of the AGV
+                        agv.setDestination(message.DestinationNode());
+                        //Copies the message to the message queue
+                        ((AGV)agv).SendMessage(message);
                         //Message was handeld so remove it
-                        messages.remove(mess);
+                        messageQueue.remove(message);
                         
                         /**
                          * 
@@ -216,32 +217,24 @@ public class Controller {
                     }
                 }
             }
-            // When a crane is requested
-            else if(mess.RequestedObject() == Crane.class)
-            {
+            // When the message requests a crane
+            else if(message.RequestedObject() == Crane.class){
                 // Check every crane in the list
                 for(Crane crane : craneList){
-                    // When the crane is available
-                    if(crane.Available()){
-                        // When the crane is assigned to this parkinglot
-                        if(crane.parkinglotTransport.node == mess.destinationNode()){
-                            // Makes a new message for the crane
-                            crane.SendMessage(new Message(
-                                    mess.DestinationObject(),
-                                    mess.Action()));
-                            
-                            // Request an AGV to fetch the first container
-                            if(mess.Action() == Message.ACTION.Unload){
-                                messages.add(new Message(
-                                    crane,
-                                    AGV.class,
-                                    Message.ACTION.Fetch));
-                            }
-                            
-                            //Message was handeld so remove it
-                            messages.remove(mess);
-                        }
-                    }
+                    // When the crane is assigned to this parkinglot
+                    if(crane.parkinglotTransport.node == message.DestinationNode()){
+                        // Makes a new message for the crane
+                        crane.SendMessage(message);                            
+                        // Request an AGV to fetch the first container
+                        if(message.GetAction() == Message.ACTION.Unload){
+                            messageQueue.add(new Message(
+                                crane,
+                                AGV.class,
+                                Message.ACTION.Fetch));
+                        }                            
+                        //Message was handeld so remove it
+                        messageQueue.remove(message);
+                    }                    
                 }
             }
         }
@@ -249,47 +242,90 @@ public class Controller {
     
     /**
      * Updates all the vehicles that still need to arrive
+     * When it arrives messageQueue will be send so the cranes will go to work
      */
     private void UpdateShipment()
     {
-        // Checks the trucks
+        // Checks if trucks arrive
         if(allTrucks.size() > 0){
-            while(simulationTime.getTime() >= allTrucks.get(0).GetArrivalDate().getTime())
-            {
-                VehicleDocking(allTrucks.get(0));
+            // When the simulation time is equal or greater than the arrivalDate
+            while(simulationTime.getTime() >= allTrucks.get(0).GetArrivalDate().getTime()){
+                // Add the truck that arrived
+                presentVehicles.add(allTrucks.get(0));
+                // Request 1 crane
+                messageQueue.add(new Message(
+                    allTrucks.get(0),
+                    Crane.class,
+                    Message.ACTION.Unload));
+                // Remove the truck that arrived
                 allTrucks.remove(0);
-                if(allTrucks.isEmpty())
+                // When there are no trucks left 
+                if(allTrucks.isEmpty()){
                     break;
+                }
             }
         }
-        // Checks the trains
+        // Checks if trains arrive
         if(allTrains.size() > 0){
-            while(simulationTime.getTime() >= allTrains.get(0).GetArrivalDate().getTime())
-            {
-                VehicleDocking(allTrains.get(0));
+            // When the simulation time is equal or greater than the arrivalDate
+            while(simulationTime.getTime() >= allTrains.get(0).GetArrivalDate().getTime()){
+                // Add the train that arrived
+                presentVehicles.add(allTrains.get(0)); 
+                // Request 2 cranes
+                for(int i = 0; i < 2; i++){
+                    messageQueue.add(new Message(
+                        allTrains.get(0),
+                        Crane.class,
+                        Message.ACTION.Unload));
+                }
+                // Removes the train that arrived
                 allTrains.remove(0);
-                if(allTrains.isEmpty())
+                // When there are no trains left
+                if(allTrains.isEmpty()){
                     break;
+                }
             }
         }
-        // Checks the barges
+        // Checks if barges arrive
         if(allBarges.size() > 0){
-            while(simulationTime.getTime() >= allBarges.get(0).GetArrivalDate().getTime())
-            {
-                VehicleDocking(allBarges.get(0));
+            // When the simulation time is equal or greater than the arrivalDate
+            while(simulationTime.getTime() >= allBarges.get(0).GetArrivalDate().getTime()){
+                // Add the barge that arrived
+                presentVehicles.add(allBarges.get(0)); 
+                // Request 4 cranes
+                for(int i = 0; i < 4; i++){
+                    messageQueue.add(new Message(
+                        allBarges.get(0),
+                        Crane.class,
+                        Message.ACTION.Unload));
+                }
+                // Removes the barge that arrived
                 allBarges.remove(0);
-                if(allBarges.isEmpty())
+                // When there are no barges left
+                if(allBarges.isEmpty()){
                     break;
+                }
             }
         }
-        // Checks the seaShips
+        // Checks if seaShips arrive
         if(allSeaShips.size() > 0){
-            while(simulationTime.getTime() >= allSeaShips.get(0).GetArrivalDate().getTime())
-            {
-                VehicleDocking(allSeaShips.get(0));
+            // When the simulation time is equal or greater than the arrivalDate
+            while(simulationTime.getTime() >= allSeaShips.get(0).GetArrivalDate().getTime()){
+                // Add the seaShip that arrived
+                presentVehicles.add(allSeaShips.get(0));
+                // Request 10 cranes
+                for(int i = 0 ; i < 10; i++){
+                    messageQueue.add(new Message(
+                        allSeaShips.get(0),
+                        Crane.class,
+                        Message.ACTION.Unload));
+                }
+                // Removes the seaShip that arrived
                 allSeaShips.remove(0);
-                if(allSeaShips.isEmpty())
+                // When there are no seaShips left
+                if(allSeaShips.isEmpty()){
                     break;
+                }
             }
         }
     }
@@ -297,7 +333,7 @@ public class Controller {
     
     /**
      * Checks all the containers that are on top of each stack
-     * When it needs to be transported it sends a message
+     * When it needs to be transported it will sends a message
      * @throws Exception 
      */
     private void FetchContainers() throws Exception
@@ -309,7 +345,7 @@ public class Controller {
                     // Checks if the cotainer needs to be transported
                     if(storageArea.PeekContainer(column, row).getDepartureDateStart().getTime() <= simulationTime.getTime()){                    
                         // Adds a fetch message for an AGV
-                        messages.add(new Message(
+                        messageQueue.add(new Message(
                                 storageArea.getClass(),
                                 AGV.class,
                                 Message.ACTION.Fetch,
@@ -322,25 +358,24 @@ public class Controller {
        
     
     /**
-     * When a vehicle dock's
-     * @param vehicle 
+     * When a vehicle arrives in the harbor
+     * @param vehicle The vehicle that arrives in the border
      */
-    public void VehicleDocking(Vehicle vehicle)
+    public void VehicleArrives(Vehicle vehicle)
     {      
-        // Add the docked Vehicle
-        dockedVehicles.add(vehicle);
-        // Interger that holds the amount of cranes this vehicle can hold for loading and unloading
-        int craneRequests = 0;
+        // Add the arrived vehicle to the present Vehicles 
+        presentVehicles.add(vehicle);
+        // Holds the amount of cranes the arrived vehicle can hold for loading and unloading
+        int cranesRequested = 0;
         
-        //When this vehicle is a Train
-        if(vehicle.getClass() == Train.class)
-        {
-            // Train's can hold 2 cranes
-            craneRequests = 2;
+        //When the vehicle is a train
+        if(vehicle.getClass() == Train.class){
+            // A train can hold 2 cranes
+            cranesRequested = 2;
         }
-        // When this vehicle is a Boat
-        else if (vehicle.getClass() == Boat.class)
-        {            
+        // When the vehicle is a boat
+        else if (vehicle.getClass() == Boat.class){            
+            
             //TODO Boat crane request
             //TODO 2 different types of boat's a SeaShip, Barge
         }
@@ -348,12 +383,12 @@ public class Controller {
         else if (vehicle.getClass() == Truck.class)
         {
             // Truck's can hold 1 crane
-            craneRequests = 1;
+            cranesRequested = 1;
         }
         
         // While the vehicle can hold more cranes            
-        while(craneRequests-- > 0){
-            messages.add(new Message(
+        while(cranesRequested-- > 0){
+            messageQueue.add(new Message(
                  vehicle,
                  Crane.class,
                  Message.ACTION.Unload));
@@ -385,24 +420,31 @@ public class Controller {
     }
     
     /**
-     * Get's the next shipment time
+     * Gets the date when the next shipment arrives
      */
     private void GetNextShipmentTime()
     {
+        // When there are still seaShips that need to arrive
         if(!allSeaShips.isEmpty()){
             nextShipment = allSeaShips.get(0).GetArrivalDate();
         }
+        // When there are still barges that need to arrive
         if(!allBarges.isEmpty()){
+            // When the first barge arrives earlier than the other shipment
             if(nextShipment.getTime() > allBarges.get(0).GetArrivalDate().getTime()){
                 nextShipment = allBarges.get(0).GetArrivalDate();
             }
         }
+        // When there are still trains that need to arrive
         if(!allTrains.isEmpty()){
+            // When the first train arrives earlier than the other shipment
             if(nextShipment.getTime() > allTrains.get(0).GetArrivalDate().getTime()){
                 nextShipment = allTrains.get(0).GetArrivalDate();
             }
         }
+        // When there are still trucks that need to arrive
         if(!allTrucks.isEmpty()){
+            // When the first truck arrives earlier than the other shipment
             if(nextShipment.getTime() > allTrucks.get(0).GetArrivalDate().getTime()){
                 nextShipment = allTrucks.get(0).GetArrivalDate();
             }
