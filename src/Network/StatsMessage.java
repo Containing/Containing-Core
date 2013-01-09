9d58191a2d5b3193a4ac6c49a6fdc235e3ab2577
Christiaan Kras <c.kras@pcc-online.net>
2013-01-09 16:29:53 +0100

    ZMQ Stats publisher example

---
+++ src/Network/StatsMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Network;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ * StatsMessage object
+ * @author Christiaan
+ */
+public class StatsMessage implements Serializable {
+    public long containers_incoming;
+    public long containers_outgoing;
+    public HashMap<String, Integer> areas = new HashMap<String, Integer>();
+}
