86d2231a75a0d9bff2c9de02332072ede5d0d387
tonnie <raket124@hotmail.com>
2013-01-10 12:16:39 +0100

    added simple class

---
+++ src/Crane/Id_Position.java
@@ -0,0 +1,17 @@
+package Crane;
+
+import Helpers.Vector3f;
+
+/**
+ *
+ * @author Tonnie Boersma
+ */
+public class Id_Position {
+    public String ID;
+    public Vector3f position;
+    
+    public Id_Position(String ID,Vector3f position){
+        this.ID = ID;
+        this.position = position;
+    }
+}
