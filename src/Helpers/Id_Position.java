02c4382bac0a1f6f50fcaa736dd68d00beb367c5
tonnie <raket124@hotmail.com>
2013-01-23 12:54:35 +0100

    Container, Vector3f
    Added a get/set Position for the position in the database
    Added some constructors in the Vector3f class

--- src/Helpers/Id_Position.java
+++ src/Helpers/Id_Position.java
@@ -10,6 +10,11 @@ public class Id_Position {
     public String ID;
     public Vector3f position;
     
+    public Id_Position(){
+        this.ID = "dummieId";
+        this.position = new Vector3f();
+    }
+    
     public Id_Position(String ID,Vector3f position){
         this.ID = ID;
         this.position = position;
